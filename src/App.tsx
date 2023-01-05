3e2d58386999af59ae660a6fdb032ed82d49e9e8
sujitbidawe <bidawesujit@gmail.com>
2023-01-05 15:57:06 +0530

    ADDED: router to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
 import './App.css';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Header from './components/header/Header';
 import Homepage from './pages/homepage/Homepage';
+import PagenotFound from './pages/pagenotfound/Pagenotfound';
 
 function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="*" element={<PagenotFound />} />
+      </Routes>
     </div>
   );
 }
